1b8286cd16e2d5a46e5119d2629cb0717f52f798
DimitrisDavidGerokonstantis <106912404+DimitrisDavidGerokonstantis@users.noreply.github.com>
2024-02-10 20:40:05 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,6 +7,8 @@ import multer from "multer";
 
 const app = express();
 
+app.use(cors(["http://localhost:3000","https://pollar-r73k.onrender.com"]));
+
 app.use(express.json());
 //app.use(cookieParser());
 
