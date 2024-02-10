f45a166c97a797573b0d4978d230634a165321b9
DimitrisDavidGerokonstantis <106912404+DimitrisDavidGerokonstantis@users.noreply.github.com>
2024-02-10 21:27:06 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ import multer from "multer";
 
 const app = express();
 
-app.use(cors({origin: ["http://localhost:3000","https://pollar-r73k.onrender.com"]}));
+app.use(cors({origin: ["http://localhost:3000","https://pollar-dzrh.onrender.com"]}));
 
 //app.use(cors({origin: ["http://localhost:3000","https://pollar-dzrh.onrender.com"]}));
 app.use(express.json());
