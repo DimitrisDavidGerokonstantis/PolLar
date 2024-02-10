d64dddb698a54fa42c1af3c44b88f367601e4f81
DimitrisDavidGerokonstantis <106912404+DimitrisDavidGerokonstantis@users.noreply.github.com>
2024-02-10 22:05:56 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ import multer from "multer";
 
 const app = express();
 
-app.use(cors({origin: ["http://localhost:3000","https://pollar-dzrh.onrender.com/participant"]}));
+app.use(cors());
 
 //app.use(cors({origin: ["http://localhost:3000","https://pollar-dzrh.onrender.com"]}));
 app.use(express.json());
