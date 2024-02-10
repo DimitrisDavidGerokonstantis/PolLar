227f6306cf596016ca0f41b169589704c00697e8
DimitrisDavidGerokonstantis <106912404+DimitrisDavidGerokonstantis@users.noreply.github.com>
2024-02-10 20:42:23 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import participantRoutes from "./routes/participant.js";
 import adminRoutes from "./routes/admin.js";
+import cors from "cors";
 
 //import cookieParser from "cookie-parser";
 import multer from "multer";
 
 const app = express();
 
-app.use(cors(["http://localhost:3000","https://pollar-r73k.onrender.com"]));
+app.use(cors({origin: ["http://localhost:3000","https://pollar-r73k.onrender.com"]}));
 
 app.use(express.json());
 //app.use(cookieParser());
