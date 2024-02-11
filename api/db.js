4f202e3a56192c1632ead91f31c6af41afaf944d
DimitrisDavidGerokonstantis <106912404+DimitrisDavidGerokonstantis@users.noreply.github.com>
2024-02-11 13:50:09 +0200

    Update db.js

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ import mysql from "mysql";
 
 export const db = mysql.createConnection({
   host: "boeebl6hmohkrw1kppr0-mysql.services.clever-cloud.com",
-  user: "u22vludggvcqmbpx",
+  user: "u22vludggvcqmbpx@91.208.207.95",
   password: "BDlgumlL1rqj3fH9K43Y",
   database: "boeebl6hmohkrw1kppr0",
 });
