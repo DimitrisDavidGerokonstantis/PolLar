b13a6361b0885d3e44f4f7f1faac70977de42db1
DimitrisDavidGerokonstantis <106912404+DimitrisDavidGerokonstantis@users.noreply.github.com>
2024-02-11 13:27:36 +0200

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,11 +1,6 @@
 import mysql from "mysql";
 
-export const db = mysql.createConnection({
-  host: "boeebl6hmohkrw1kppr0-mysql.services.clever-cloud.com",
-  user: "u22vludggvcqmbpx",
-  password: "BDlgumlL1rqj3fH9K43Y",
-  database: "boeebl6hmohkrw1kppr0",
-});
+export const db = mysql.createConnection("mysql://u22vludggvcqmbpx:BDlgumlL1rqj3fH9K43Y@boeebl6hmohkrw1kppr0-mysql.services.clever-cloud.com:3306/boeebl6hmohkrw1kppr0");
 
 //db.query("CREATE USER 'u22vludggvcqmbpx'@'34.213.214.55' IDENTIFIED BY 'BDlgumlL1rqj3fH9K43Y'");
 //db.query("GRANT ALL PRIVILEGES ON boeebl6hmohkrw1kppr0.* TO 'u22vludggvcqmbpx'@'34.213.214.55'");
