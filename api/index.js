5d9a124510c61244e617403d6ab277e07842860f
Dimitris Gerokonstantis <106912404+DimitrisDavidGerokonstantis@users.noreply.github.com>
2024-09-12 18:49:58 +0300

    Update CORS

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ import multer from "multer";
 const app = express();
 
 const corsOptions = {
-  origin: ["*", "http://localhost:4200"],
+  origin: ["https://pollar-app.onrender.com", "http://localhost:4200"],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
