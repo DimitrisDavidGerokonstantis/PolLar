53027507a60479038dda914fb7f57db24ed95949
Dimitris Gerokonstantis <106912404+DimitrisDavidGerokonstantis@users.noreply.github.com>
2024-09-12 18:44:10 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ import multer from "multer";
 const app = express();
 
 const corsOptions = {
-  origin: "*",
+  origin: ["*", "http://localhost:4200"],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
