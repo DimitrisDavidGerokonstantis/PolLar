a325a5abed50ad310fe8911ec71b9195c5022474
DimitrisDavidGerokonstantis <dimitrisger2@gmail.com>
2024-12-30 00:34:57 +0200

    Fix Frontend SSR version

--- client-V2-SSR/server.ts
+++ client-V2-SSR/server.ts
@@ -1,41 +1,60 @@
-import 'zone.js/node'; // Required for Angular Universal
-import { enableProdMode } from '@angular/core';
-import { ngExpressEngine } from '@nguniversal/express-engine';
-import * as express from 'express';
-import * as cors from 'cors'; // Import the CORS middleware
-import { join } from 'path';
-import { AppServerModule } from './src/main.server';
-import { AppServerModuleNgFactory } from './src/main.server.ngfactory';
+import { APP_BASE_HREF } from '@angular/common';
+import { CommonEngine } from '@angular/ssr';
+import express from 'express';
+import { fileURLToPath } from 'node:url';
+import { dirname, join, resolve } from 'node:path';
+import bootstrap from './src/main.server';
 
-// Enable production mode for faster rendering
-enableProdMode();
+// The Express app is exported so that it can be used by serverless Functions.
+export function app(): express.Express {
+  const server = express();
+  const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+  const browserDistFolder = resolve(serverDistFolder, '../browser');
+  const indexHtml = join(serverDistFolder, 'index.server.html');
 
-const app = express();
-const distFolder = join(process.cwd(), 'dist/my-angular-ssr-app/browser');
+  const commonEngine = new CommonEngine();
 
-// Enable CORS for all origins (you can restrict this if needed)
-app.use(cors());
+  server.set('view engine', 'html');
+  server.set('views', browserDistFolder);
 
-// Use Angular Universal's express engine
-app.engine(
-  'html',
-  ngExpressEngine({
-    bootstrap: AppServerModuleNgFactory,
-  })
-);
+  // Example Express Rest API endpoints
+  // server.get('/api/**', (req, res) => { });
+  // Serve static files from /browser
+  server.get(
+    '**',
+    express.static(browserDistFolder, {
+      maxAge: '1y',
+      index: 'index.html',
+    })
+  );
 
-app.set('view engine', 'html');
-app.set('views', distFolder);
+  // All regular routes use the Angular engine
+  server.get('**', (req, res, next) => {
+    const { protocol, originalUrl, baseUrl, headers } = req;
 
-// Serve static files from /browser
-app.get('*.*', express.static(distFolder));
+    commonEngine
+      .render({
+        bootstrap,
+        documentFilePath: indexHtml,
+        url: `${protocol}://${headers.host}${originalUrl}`,
+        publicPath: browserDistFolder,
+        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+      })
+      .then((html) => res.send(html))
+      .catch((err) => next(err));
+  });
 
-// All regular routes use the Universal engine
-app.all('*', (req, res) => {
-  res.render('index', { req });
-});
+  return server;
+}
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log(`Node server listening on http://localhost:${port}`);
-});
+function run(): void {
+  const port = process.env['PORT'] || 4000;
+
+  // Start up the Node server
+  const server = app();
+  server.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
+}
+
+run();
